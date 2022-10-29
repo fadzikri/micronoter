7246b8b2bd29815afc40b027c96844b1b37bae31
fadzikri <109416385+fadzikri@users.noreply.github.com>
2022-10-29 16:46:26 +0700

    Simpan

--- src/scripts/detail.js
+++ src/scripts/detail.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import axios from "axios";
 import { url, cariTempat } from "./smartphone";
 
@@ -20,124 +21,75 @@ const loadIdCari = async () => {
 };
 
 const detailSpesifikasi = async (data) => {
-  let {
-    brand,
-    dimension,
-    os,
-    phone_images,
-    phone_name,
-    release_date,
-    specifications,
-    storage,
-  } = data;
+  return (cariTempat.innerHTML = `<h5>Jaringan</h5>
+  ${await extract(data)}
+  `);
+};
 
-  const [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14] =
-    specifications;
-  const jaringan = s1.specs[0].val[0];
-  const status = s2.specs[1].val[0];
-  const bodyDimensi = s3.specs[0].val[0];
-  const bodyBerat = s3.specs[1].val[0];
-  const bodyTerbuat = s3.specs[2].val[0];
-  const bodySim = s3.specs[3].val[0];
-  const bodyLainnya = s3.specs[4].val[0];
-  const displayTipe = s4.specs[0].val[0];
-  const displayResolusi = s4.specs[1].val[0];
-  const displaySize = s4.specs[2].val[0];
-  const displayUkuran = s4.specs[3].val[0];
-  const deviceOS = 
-  const deviceChip =
-  const deviceCPU =
-  const deviceGPU =
-  const
-  const
-  const
-  const
-  const
-  const
-  const
-  const
-  const
-  const
-  const
-  const
-  const
-  const
-  const
-  const
-  const
+const extract = async (data) => {
+  let { phone_images, phone_name, brand, specifications } = data;
+  let jaringan = await extractSn(specifications[0]);
+  // let status = await extractSn(specifications[1]);
+  // let body = await extractSn(specifications[2]);
+  // let display = await extractSn(specifications[3]);
+  // let device = await extractSn(specifications[4]);
+  // let memory = await extractSn(specifications[5]);
+  // let kameraBel = await extractSn(specifications[6]);
+  // let kameraDep = await extractSn(specifications[7]);
+  // let suara = await extractSn(specifications[8]);
+  // let sinyal = await extractSn(specifications[9]);
+  // let fitur = await extractSn(specifications[10]);
+  // let hal = await extractSn(specifications[11]);
+  // let baterai = await extractSn(specifications[12]);
 
+  return await extractSn(jaringan);
 
-  console.log(data);
+  // console.log(jaringan);
 
-  release_date = release_date.replace(/released|exp\.\srelease/i, "");
-  dimension = dimension.replace(/thickness/i, "");
-  storage = storage.replace(/storage/i, "");
-  storage = storage.replace(/no.*/i, "tidak ada slot SD Card");
-
-  cariTempat.innerHTML = "<h5>Detail Spesifikasi<h5>";
-  cariTempat.innerHTML += `<img src="${phone_images[0]}" alt="${phone_name}" class="gambar-hasil"><br>`;
-  cariTempat.innerHTML += `<h5>Deskripsi</h5>`;
-  cariTempat.innerHTML += `<p>Nama : ${phone_name}<br>Brand : ${brand}<br>Rilisan : ${release_date}<br>Sistem Operasi : ${os}<br>Dimensi : ${dimension}<br>Penyimpanan : ${storage}`;
-  cariTempat.innerHTML += `Jaringan</p>`;
+  // jaringan = jaringan[0][0];
+  // status = status[0][0];
+  // body = { dimensi: body[0][0], berat: body[1][0], sim: body[2][0] };
+  // display = {
+  //   tipe: display[0][0],
+  //   size: display[1][0],
+  //   resolusi: display[2][0],
+  // };
+  // device = {
+  //   os: device[0][0],
+  //   chip: device[1][0],
+  //   cpu: device[2][0],
+  //   gpu: device[3][0],
+  // };
+  // memory = {
+  //   external: memory[0][0],
+  //   internal: memory[1][0],
+  // };
+  // kameraBel = {
+  //   foto: 1,
+  //   video: 2,
+  // };
+  // kameraDep = {
+  //   foto: 1,
+  //   video: 2,
+  // };
+  // suara = {
+  //   speaker: 1,
+  //   audiojack: 2,
+  // };
+  // sinyal = {};
+  // hal = {};
+  // baterai = {};
 };
 
 const extractSn = async (Sn) => {
-  let data = new Object();
+  let res = new String();
 
-  for (i in Sn) {
-    data[i] = Sn.specs[i].val[0]
-  }
-  
-  return data;
-}
+  Sn.specs.forEach((x) => {
+    console.log(res);
+    res += `${x.key} : ${x.val[0]}<br>`;
+  });
+
+  return res;
+};
 
 export default loadIdCari;
-
-// ecifications
-// :
-// Array(14)
-// 0
-// :
-// {title: 'Network', specs: Array(6)}
-// 1
-// :
-// {title: 'Launch', specs: Array(2)}
-// 2
-// :
-// {title: 'Body', specs: Array(5)}
-// 3
-// :
-// {title: 'Display', specs: Array(5)}
-// 4
-// :
-// {title: 'Platform', specs: Array(4)}
-// 5
-// :
-// {title: 'Memory', specs: Array(3)}
-// 6
-// :
-// {title: 'Main Camera', specs: Array(3)}
-// 7
-// :
-// {title: 'Selfie camera', specs: Array(3)}
-// 8
-// :
-// {title: 'Sound', specs: Array(2)}
-// 9
-// :
-// {title: 'Comms', specs: Array(6)}
-// 10
-// :
-// {title: 'Features', specs: Array(2)}
-// 11
-// :
-// {title: 'Battery', specs: Array(2)}
-// 12
-// :
-// {title: 'Misc', specs: Array(5)}
-// 13
-// :
-// {title: 'Tests', specs: Array(6)}
-// length
-// :
-// 14
